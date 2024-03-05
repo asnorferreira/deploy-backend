4635eb6f96e55d10f83e8062fde9c5744f5043e1
asnorferreira <asnorferreira@gmail.com>
2024-03-05 18:36:24 -0300

    Add DB Postgre

--- src/controller/getList.js
+++ src/controller/getList.js
@@ -1,3 +1,11 @@
+import knex from "../config/conect.js";
+
 export const getList = async (req, res) => {
-  return res.status(200).json("Everything is allright");
+  try {
+    const car = await knex("carros").select("*");
+    return res.status(200).json(car);
+  } catch (error) {
+    console.error("Error ao listar carros: ", error);
+    return res.status(500).json(message.error);
+  }
 };
